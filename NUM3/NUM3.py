595a8bc6c65c1e22fcbdb97ba23a79abc5f078c6
mikolaj.kowalski <mikser.kowalski@gmail.com>
2024-11-08 22:44:18 +0100

    fix&feat: changed logic and added LU decomp

--- NUM3/NUM3.py
+++ NUM3/NUM3.py
@@ -1,24 +1,34 @@
 import numpy as np
 
 N = 300
-A = np.zeros((N, N))
+A = np.zeros((N, 4))
 X = np.zeros((N, 1))
-L = np.zeros((N, N))
-U = np.zeros((N, N))
+L = np.zeros((N, 2))
+U = np.zeros((N, 3))
 
 for i in range(N): 
     if i < N:
-        A[i][i] = 1.01
+        A[i][1] = 1.01
     if i < N - 1:
-        A[i + 1][i] = 0.3
-    if i < N - 1:
-        A[i][i + 1] = 0.2 / (i + 1)
+        A[i][0] = 0.3
+        A[i][2] = 0.2 / (i + 1)
     if i < N - 2:
-        A[i][i + 2] = 0.15 / (i + 1) ** 3
+        A[i][3] = 0.15 / (i + 1) ** 3
         
 for i in range(N):
     X[i][0] = i + 1
 
-np.set_printoptions(precision=4, suppress=True, linewidth=150, threshold=np.inf)
-print(L)
-#print(A)
+for i in range(N):
+    L[i][1] = 1        
+    U[i][0] = A[i][1] - L[i - 1][0] * U[i - 1][1]    
+    L[i][0] = A[i][0]/U[i][0]   
+    U[i][1] = A[i][2] - L[i - 1][0] * U[i - 1][2]
+    U[i][2] = A[i][3]
+
+detA = 1
+for i in range (N):
+    detA *= U[i][0]
+ 
+np.set_printoptions(precision=10, suppress=True, linewidth=150, threshold=np.inf)
+print(U)
+print(detA)
