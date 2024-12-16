729d28f48592c321fb5a5e6570f213cc3184c5c1
mikolaj.kowalski <mikser.kowalski@gmail.com>
2024-12-16 16:23:54 +0100

    feat: added lagrange interpolation

--- NUM7/NUM7.py
+++ NUM7/NUM7.py
@@ -22,16 +22,39 @@ def yi(x):
         
     return y
 
-n = 10
-xi = xi(n)
-yi = yi(xi)
+n = 100
+x = xi(n)
+y = yi(x)
+
+n_interp = 10
+if (n_interp + 1) % 2 != 0:
+    n_interp += 1
+x_interp = xi(n_interp)
+y_interp = yi(x_interp)
+
+def lagrange(xs, x, y, n):
+    li = []
+    Wx = []
     
-print (xi)
-print (yi)
+    for i in range(n):
+        l = 1
+        for j in range(n):
+            if i != j:
+                l *= (xs - x[j])/(x[i] - x[j])
+        
+        li.append(l)
+        
+    for i in range(n):
+        Wx.append(y[i]*li[i])
+    
+    return sum(Wx) 
+    
+lagrange_y = []
+lagrange_y = [lagrange(x, x_interp, y_interp, n_interp) for x in x]
 
-
-
-plt.plot(xi, yi, label='f(x) = 1/(1 + 10*x^2)')
+plt.plot(x, y, label='f(x) = 1/(1 + 10*x^2)')
+plt.plot(x_interp, y_interp, 'ko', label='Interpolation points')
+plt.plot(x, lagrange_y, '--',label='Lagrange Interpolation')
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.title('f(x) = 1/(1 + 10*x^2)')
